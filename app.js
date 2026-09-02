fd6a9450df5079237655601cf3885433d9ff063e
Oyetomi Oyelaja <torreyjh@gmail.com>
2026-09-02 22:07:23 +0200

    Add update recipe api method

--- app.js
+++ app.js
@@ -66,7 +66,15 @@ app.get("/recipes/:id", (req, res) => {
 
 
 //  Iteration 6 - Update a Single Recipe
-//  PUT  /recipes/:id route
+app.put("/recipes/:id", (req, res) => {
+  Recipe.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then((recipe) => {
+      res.status(200).json(recipe);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "Failed to update recipe" });
+    });
+});
 
 
 //  Iteration 7 - Delete a Single Recipe
