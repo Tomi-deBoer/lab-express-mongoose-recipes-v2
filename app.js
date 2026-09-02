fd25e01fed9db377889addb84b4bdf91e6d06485
Oyetomi Oyelaja <torreyjh@gmail.com>
2026-09-02 21:56:31 +0200

    Add get recipe api method

--- app.js
+++ app.js
@@ -42,7 +42,15 @@ app.post("/recipes", (req, res) => {
 
 
 //  Iteration 4 - Get All Recipes
-//  GET  /recipes route
+app.get("/recipes", (req, res) => {
+  Recipe.find()
+    .then((recipes) => {
+      res.status(200).json(recipes);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "Failed to get recipes" });
+    });
+});
 
 
 //  Iteration 5 - Get a Single Recipe
